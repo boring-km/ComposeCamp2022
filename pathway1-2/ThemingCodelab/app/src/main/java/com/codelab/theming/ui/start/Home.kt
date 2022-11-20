fadaa96b7e4b0d2a0768d48ffd42f0b5d26dd521
Kangmin <kms0644804@naver.com>
2022-11-20 20:19:05 +0900

    [ThemingCodelab] Jetpack Compose theming: 누락된 스타일 추가

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -1,7 +1,6 @@
 package com.codelab.theming.ui.start
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,10 +13,11 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,14 +83,13 @@ fun Header(
     Surface(
         color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
         contentColor = MaterialTheme.colors.primary,
-        modifier = Modifier
+        modifier = Modifier.semantics { heading() }
     ) {
         Text(
             text = text,
             style = MaterialTheme.typography.subtitle2,
             modifier = modifier
                 .fillMaxWidth()
-                .background(Color.LightGray)
                 .padding(horizontal = 16.dp, vertical = 8.dp)
         )
     }
@@ -162,6 +161,7 @@ private fun PostMetadata(
     CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
         Text(
             text = text,
+            style = MaterialTheme.typography.body2,
             modifier = modifier
         )
     }
